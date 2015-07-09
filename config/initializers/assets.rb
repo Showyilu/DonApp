d9522ccedad06144cca13cd28d6cf872d88832aa
xiaoyi <xiaoyi.lu@corporategovernancerisk.com>
2015-07-09 12:55:07 +0000

    Delete more

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,26 +13,13 @@ Rails.application.config.assets.precompile += %w(
   javascripts/style-switcher/color-switcher.css
   javascripts/mainmenu/bootstrap.min.css
   javascripts/mainmenu/menu.css
-  javascripts/masterslider/style/masterslider.css
-  javascripts/masterslider/skins/default/style.css
   javascripts/carouselowl/owl.transitions.css
   javascripts/carouselowl/owl.carousel.css
   javascripts/cubeportfolio/cubeportfolio.min.css
   javascripts/form/sky-forms.css
   javascripts/tabs/assets/css/responsive-tabs2.css
   colors/blue.css
-  colors/green.css
-  colors/cyan.css
-  colors/orange.css
-  colors/lightblue.css
-  colors/pink.css
-  colors/purple.css
-  colors/bridge.css
-  colors/slate.css
-  colors/yellow.css
-  colors/darkred.css
   masterslider/jquery.easing.min.js
-  masterslider/masterslider.min.js
   mainmenu/customeUI.js
   mainmenu/bootstrap.min.js
   universal/jquery.js
@@ -40,8 +27,6 @@ Rails.application.config.assets.precompile += %w(
   mainmenu/sticky.js
   form/jquery.form.min.js
   form/jquery.validate.min.js
-  cubeportfolio/jquery.cubeportfolio.min.js
-  cubeportfolio/main.js
   carouselowl/owl.carousel.js
   tabs/assets/js/responsive-tabs.min.js
   universal/custom.js
